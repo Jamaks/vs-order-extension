ecbdfb2dbdabaaf34eb680dc23ffeb4b9f528a99
Jamak <jamarust@mail.ru>
2017-05-06 12:43:25 +0300

    add shipmentModule blades

---
+++ Scripts/ShipmentModule.js
@@ -0,0 +1,46 @@
+﻿//Call this to register our module to main application
+var moduleName = "Jamak.ShipmentModule";
+
+if (AppDependencies != undefined) {
+	AppDependencies.push(moduleName);
+}
+
+var ShipmentModule= angular.module(moduleName, [
+    
+])
+.config(
+	['$stateProvider',
+		function ($stateProvider) {
+			$stateProvider
+				.state('workspace.shipment', {
+					url: '/shipment',
+					templateUrl: 'Scripts/common/templates/home.tpl.html',
+					controller: [
+						'$scope', 'platformWebApp.bladeNavigationService', function ($scope, bladeNavigationService) {
+							var blade = {
+								id: 'shipmentsList',
+								// controller name must be unique in Application. Use prefix like 'um-'.
+								controller: 'Jamak.ShipmentModule.shipmentsListController',
+								template: 'Modules/Order.Shipment/Scripts/blades/shipmentsList.tpl.html',
+								isClosingDisabled: true
+							};
+							bladeNavigationService.showBlade(blade);
+						}
+					]
+				});
+		}
+	]
+)
+.run(
+	['$rootScope', 'platformWebApp.mainMenuService','$state', function ($rootScope, mainMenuService,$state) {
+ var menuItem = {
+            path: 'browse/shipment',
+            icon: 'fa fa-truck',
+            title: 'Shipment',
+            priority: 90,
+            action: function () { $state.go('workspace.shipment'); },
+            permission: 'order:access'
+        };
+        mainMenuService.addMenuItem(menuItem);
+	}])
+
---
+++ Scripts/blades/shipmentsList.js
@@ -0,0 +1,14 @@
+﻿ShipmentModule.controller('Jamak.ShipmentModule.shipmentsListController', ['$scope', '$http', function ($scope, $http) {
+    var blade = $scope.blade;
+
+    $scope.data = "Shipment content";
+    blade.title = "Shipment module";
+    blade.isLoading = true;
+    $http.post("/api/order/delivery/search", { "sort": "createdDate:desc", "skip": 0, "take": 20 })
+        .then(function success(resp) {
+            console.log(resp.data)
+            blade.isLoading = false;
+        }, function error(resp) {
+            blade.isLoading = false;
+        });
+}]);
